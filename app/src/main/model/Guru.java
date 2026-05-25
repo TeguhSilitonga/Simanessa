ce274bccd4ca43ea2d2e0628eadc6d31c82e28a1
TeguhSilitonga <teguhsilitonga657@gmail.com>
2026-05-25 14:32:05 +0800

    Tambahkan file  Guru

--- app/src/main/model/Guru.java
+++ app/src/main/model/Guru.java
@@ -1,3 +1,13 @@
-public class Guru {
-    
+package model;
+
+public class Guru extends User {
+
+    public Guru(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public String showRole() {
+        return "Guru";
+    }
 }
