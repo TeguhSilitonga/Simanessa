488d0890d31f9507f1e03147a6b87062c5366b91
TeguhSilitonga <teguhsilitonga657@gmail.com>
2026-05-25 14:51:36 +0800

    Tambahkan Weightconfig

--- app/src/main/model/WeightConfig.java
+++ app/src/main/model/WeightConfig.java
@@ -1,3 +1,19 @@
+package model;
+
 public class WeightConfig {
-    
+
+    public static double utsWeight = 30;
+
+    public static double uasWeight = 40;
+
+    public static double tugasWeight = 30;
+
+    public static boolean isValid() {
+
+        return
+                (utsWeight
+                        + uasWeight
+                        + tugasWeight)
+                        == 100;
+    }
 }
