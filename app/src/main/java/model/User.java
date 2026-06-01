d0c5a4118297b38fb1e7211e93c4b21d69b36dd7
TeguhSilitonga <teguhsilitonga657@gmail.com>
2026-06-01 10:12:41 +0800

    MemperbaikiUser

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -1,22 +1,45 @@
 package model;
 
-public abstract class User {
-
+public class User {
     protected String username;
     protected String password;
+    protected String role; // Variabel baru untuk membedakan "Admin" dan "Guru"
 
-    public User(String username, String password) {
+    // Constructor yang diminta oleh Guru.java dan LoginView.java
+    public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
+        this.role = role;
     }
 
-    public String getUsername() {
-        return username;
+    // Getter dan Setter
+    public String getUsername() { 
+        return username; 
+    }
+    
+    public void setUsername(String username) { 
+        this.username = username; 
     }
 
-    public String getPassword() {
-        return password;
+    public String getPassword() { 
+        return password; 
+    }
+    
+    public void setPassword(String password) { 
+        this.password = password; 
     }
 
-    public abstract String showRole();
+    // Fungsi getRole() yang sebelumnya dicari oleh VS Code dan tidak ketemu
+    public String getRole() { 
+        return role; 
+    }
+    
+    public void setRole(String role) { 
+        this.role = role; 
+    }
+
+    // Fungsi showRole() untuk ditampilkan di UI Sidebar
+    public String showRole() {
+        return role;
+    }
 }
